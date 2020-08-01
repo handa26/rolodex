c2d00e588aae52e36f830d0a6454c7c7f8a60130
Ananda Muhammad Muthaqin <anandamuhammadmtq@gmail.com>
2020-08-02 00:20:59 +0800

    Updates

---
+++ precache-manifest.0b0f6a65024093a35481d636b5a3c9b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f6835df8986ee932f67894aedfa219b",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "e6666fbd2704fa7455a9",
+    "url": "/rolodex/static/css/main.208239ca.chunk.css"
+  },
+  {
+    "revision": "78b9060f1495d3485a95",
+    "url": "/rolodex/static/js/2.33b4488b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rolodex/static/js/2.33b4488b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6666fbd2704fa7455a9",
+    "url": "/rolodex/static/js/main.340342dd.chunk.js"
+  },
+  {
+    "revision": "604800cde4d3fb0ab0cb",
+    "url": "/rolodex/static/js/runtime-main.b7003d66.js"
+  }
+]);
